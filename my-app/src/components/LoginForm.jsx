8a44fd2be2563e17f3bc8073c3fe7cc69c901ace
baltej3839 <baltej3839@github.com>
2025-11-11 20:37:49 +0530

    login

---
+++ my-app/src/components/LoginForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function LoginForm() {
+const [name, setName] = useState('');
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState(''); 
+
+const [submitted, setSubmitted] = useState(false);
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+}
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit">Login</button>
+
+                {submitted && (
+                    <div>
+                        <p>Name: {name}</p>
+                        <p>Email: {email}</p>
+                        <p>Password: {password}</p>
+                    </div>
+                )}
+            </form>
+        </>
+    );
+}
